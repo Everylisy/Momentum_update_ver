37738a7a86f4049e8173653a18e350032af38304
violetwhenisawu <60170829+violetwhenisawu@users.noreply.github.com>
2022-02-10 00:20:06 +0900

    Add files via upload
    add weather (By weather API)

--- JS/weather.js
+++ JS/weather.js
@@ -3,8 +3,15 @@ const Api_KEY = "b3025b2adc51535575fb5f1a9b6bee97";
 function onGeoGood(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${Api_KEY}`;
-    console.log(URL);
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${Api_KEY}&units=metric`;
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoBad() {
