f600e8ec662d52d7b25a6e147f8296f2363d70e0
violetwhenisawu <60170829+violetwhenisawu@users.noreply.github.com>
2022-02-18 00:53:53 +0900

    Update todo.js
    finished To_Do list

--- JS/todo.js
+++ JS/todo.js
@@ -2,23 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+let toDos = [];
 
 function saveTodos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 
 function deleteToDo(event) {
     const deleteLi = event.target.parentElement;
     deleteLi.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteLi.id));
+    saveTodos();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
 
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
 
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -34,9 +38,22 @@ function handleToDoSumbmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObject = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObject);
+    paintToDo(newTodoObject);
     saveTodos();
 };
 
 toDoForm.addEventListener("submit", handleToDoSumbmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if(savedTodos) {
+    const parsedToDos = JSON.parse(savedTodos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
